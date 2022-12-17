b4f2caf2bd0e7fd2c37def80144f18b2a83f0d70
[mahmoudk07] <mahmoudkk177@gmail.com>
2022-12-17 03:44:47 +0200

    HPF with log and perf files

--- scheduler.c
+++ scheduler.c
@@ -69,10 +69,10 @@ int main(int argc, char *argv[])
     mode = atoi(argv[2]);
 
     /*Semaphore Variables*/
-    Count_OF_Processes = atoi(argv[4]); /*count of processes in system*/
-    key_t semaphore_Keys[Count_OF_Processes];     /*array of semaphore keys where each process has a semaphore key*/
-    union Semun semun;                            /*Semaphpre union*/
-    semaphore_IDs = (int*)malloc(Count_OF_Processes*sizeof(int));
+    Count_OF_Processes = atoi(argv[4]);       /*count of processes in system*/
+    key_t semaphore_Keys[Count_OF_Processes]; /*array of semaphore keys where each process has a semaphore key*/
+    union Semun semun;                        /*Semaphpre union*/
+    semaphore_IDs = (int *)malloc(Count_OF_Processes * sizeof(int));
     // for (int i = 0; i < 8; i++)  intialize semaphore ids by -1 at first
     // {
     //     semaphore_IDs[i] = -1;
@@ -241,23 +241,21 @@ int main(int argc, char *argv[])
 
             /*Make rec_value = 1 to re-enter the check recieved loop again*/
             rec_value = 1;
-        /*IF Schedular finished its job it Terminates*/
-        if (finished_process_count == Count_OF_Processes)
-        {
-            //if(mode== SJF){follow++;cpu_waiting_time++;}
-            char line_to_print[256];
-            
-            sprintf(line_to_print, "\nCPU utilization = %.2f %%\nAvg WTA = %.2f\nAvg Waiting = %.2f \n", ((float)(follow - cpu_waiting_time) * 100) / (float)follow, total_WTA_time / Count_OF_Processes, (float)total_waiting_time / (float)Count_OF_Processes);
-            fputs(line_to_print, CPU_file);
-            printf("cpu_wainting_time= %d , curr_time = %d",cpu_waiting_time , follow);
-            fclose(CPU_file);
-            fclose(processess_file);
+            /*IF Schedular finished its job it Terminates*/
+            if (finished_process_count == Count_OF_Processes)
+            {
+                // if(mode== SJF){follow++;cpu_waiting_time++;}
+                char line_to_print[256];
 
-            kill(getppid(),SIGINT);
+                sprintf(line_to_print, "\nCPU utilization = %.2f %%\nAvg WTA = %.2f\nAvg Waiting = %.2f \n", ((float)(follow - cpu_waiting_time) * 100) / (float)follow, total_WTA_time / Count_OF_Processes, (float)total_waiting_time / (float)Count_OF_Processes);
+                fputs(line_to_print, CPU_file);
+                printf("cpu_wainting_time= %d , curr_time = %d", cpu_waiting_time, follow);
+                fclose(CPU_file);
+                fclose(processess_file);
+
+                kill(getppid(), SIGINT);
+            }
         }
-        }
-
-
     }
     // TODO: implement the scheduler.
     // TODO: upon termination release the clock resources.
@@ -440,6 +438,8 @@ void HPF_Algo(int *Process_Semaphore, int Time)
         calc_Proc_waiting_Prio(&Priority_List_HPF_SJF);            /*Function to increase waiting time for not runing processes in ready queue*/
         --Priority_List_HPF_SJF.head->Process_Data.Remaining_time; /*decrease remaining time for the runing process*/
     }
+    if (isPriorityQueueEmpty(&Priority_List_HPF_SJF))
+        cpu_waiting_time++;
 }
 
 void SJF_Algo(int *Process_Semaphore, int Time)
@@ -540,15 +540,15 @@ void SJF_Algo(int *Process_Semaphore, int Time)
 */
 void handler(int signum)
 {
-     if(finished_process_count != Count_OF_Processes)
-     {
-            char line_to_print[256];
-            sprintf(line_to_print, "\nCPU utilization = %.2f %%\nAvg WTA = %.2f\nAvg Waiting = %.2f \n", ((float)cpu_waiting_time * 100) / (float)follow, total_WTA_time / Count_OF_Processes, (float)total_waiting_time / (float)Count_OF_Processes);
-            fputs(line_to_print, CPU_file);
-            fclose(CPU_file);
-            fclose(processess_file);
+    if (finished_process_count != Count_OF_Processes)
+    {
+        char line_to_print[256];
+        sprintf(line_to_print, "\nCPU utilization = %.2f %%\nAvg WTA = %.2f\nAvg Waiting = %.2f \n", ((float)cpu_waiting_time * 100) / (float)follow, total_WTA_time / Count_OF_Processes, (float)total_waiting_time / (float)Count_OF_Processes);
+        fputs(line_to_print, CPU_file);
+        fclose(CPU_file);
+        fclose(processess_file);
     }
-    //printf("\nSCh: ana matt with CPU_WT = %d , TOTAL_WTA = %.2f, TOTAL_Processess_WT = %d \n", cpu_waiting_time, total_WTA_time, total_waiting_time);
+    // printf("\nSCh: ana matt with CPU_WT = %d , TOTAL_WTA = %.2f, TOTAL_Processess_WT = %d \n", cpu_waiting_time, total_WTA_time, total_waiting_time);
     for (size_t i = 0; i < 8; i++)
     {
         semctl(semaphore_IDs[i], 0, IPC_RMID, NULL);
